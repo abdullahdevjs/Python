1b56b0147e0f951abc7333a4ec9e7fae4907b916
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-03-12 22:42:02 +0530

    assignment question

--- A2.py
+++ A2.py
@@ -1,2 +1,3 @@
-import A1
-print(A1.x) # . is a oprator jo btata h ki A1 ke ander se x ko access krna h
+from  A1 import x
+x=12
+print(x) # . is a oprator jo btata h ki A1 ke ander se x ko access krna h
---
+++ assign1.py
@@ -0,0 +1,2 @@
+print('"abdullah"') # this will print abdullah with double quote
+print("'Sumaiya'") # this will print sumaiya with single quote
