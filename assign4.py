efea12031891cb3c0e81f8545dec51b968cbb799
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-03-14 15:52:36 +0530

    assignment

---
+++ assign4.py
@@ -0,0 +1,7 @@
+# write a python script to calculate the simple interest
+print(" Enter p,r and t")
+p=float(input())
+r=float(input())
+t=float(input())
+si=(p*r*t)/100
+print("result=",si)
---
+++ assing3.py
@@ -0,0 +1,4 @@
+# write a python program to print the square of number from the user
+a=int(input("enter the number"))
+b=a**2
+print("square of a number",a,"is",b, sep=" ")
