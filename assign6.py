9b177ead1a5fd0706db19d2f707ccba2c6c78b61
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-03-14 23:18:23 +0530

    grade

---
+++ assign6.py
@@ -0,0 +1,16 @@
+marks=int(input("enter the marks"))
+if marks>90 and marks<=100:
+    print("grade A")
+elif marks>80 and marks<=90:
+    print("grade B")
+elif marks>70 and marks<=80:
+    print("grade C")
+elif marks>60 and marks<=70:
+    print("Grade D")
+elif marks>=50 and marks<=60:
+    print("grade E")
+else:
+    print("fail")
+
+
+    
