37564f1512da686898a1e032cc6a78fcc61779ff
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-03-14 22:54:26 +0530

    condition

---
+++ assign5.py
@@ -0,0 +1,6 @@
+# check the given number is positive or non positive
+x=int(input("enter a number"))
+if x>0:
+    print("given number is positive")
+else:
+    print("given number is non positive")
