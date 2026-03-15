177437969c479e9e50652652757a4ae96c6031bf
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-03-15 15:58:22 +0530

    three digit check

---
+++ assign7.py
@@ -0,0 +1,7 @@
+# program to print gthe given number is three digit or not
+n=int(input("enter a number"))
+if n > 99 and n < 1000:
+    print("given number is three digit number")
+else:
+    print("given number is not three digit number")
+    
