409452446867ebe600b7ee06d8640da2ac387637
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-04-25 21:33:21 +0530

    vowel and space in string

--- assign22.py
+++ assign22.py
@@ -3,3 +3,12 @@ s=input("enter a string")
 for ch in s:
     if ch in "aeiouAEIOU":
         print(ch)
+
+
+#Q count spaces in a string
+s=input("enter a string")
+count=0
+for ch in s:
+    if ch==' ':
+        count+=1
+print("count=",count)
