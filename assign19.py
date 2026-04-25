d6ef22a9c42563d90fb9543f2cf458756db22b8e
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-04-25 14:06:13 +0530

    sum and sqaure sum of user input

--- assign19.py
+++ assign19.py
@@ -4,3 +4,10 @@ s=0
 for i in range(1, n+1):
     s=s+i
 print("sum is",s)
+
+#Q2 square of first n natural number
+n=int(input("enter a  number"))
+s=0
+for i in range(1,n+1):
+    s=s+i**2
+print("sum of square",s)
