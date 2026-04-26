0fe090b43b49e90e4d2e5d48708fbd1913b7226a
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-04-26 12:58:59 +0530

    multiple on user input

---
+++ assign23.py
@@ -0,0 +1,10 @@
+#first 10 multiple of 5
+n=int(input("enter a  number"))
+for i in range(1,n+1):
+    print(5*i)
+
+#Q python script to print first m multiple of n
+n=int(input("enter a number"))
+m=int(input("how many multiple you want"))
+for i in range(1,m+1):
+    print(n*i)
