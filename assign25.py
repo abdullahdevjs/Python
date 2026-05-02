67539dbc7758a4fb49b2177062722eebb3bd2fe3
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-05-02 21:28:55 +0530

    even no in a list and fabinocii series

--- assign25.py
+++ assign25.py
@@ -1,3 +1,16 @@
 #python script to print first n even number in a lis
 l1=[2*x for x in range(1,int(input("enter the number"))+1)]
 print(l1)
+
+#Q2 fabinocii series in a list
+n=int(input("enter a number"))
+fib=[]
+a=-1
+b=1
+while n:
+    c=a+b
+    fib.append(c)
+    a=b
+    b=c
+    n-=1
+print(fib)
