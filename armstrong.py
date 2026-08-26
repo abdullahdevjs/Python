e7132f87d04703ade8b502d34f0253c9740803be
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-08-27 00:51:55 +0530

    armstrong number within a range

--- armstrong.py
+++ armstrong.py
@@ -10,3 +10,20 @@ while number > 0:
 
 if s == original:
     print("Given number is Armstrong")
+
+number1 = int(input("enter number"))
+number2 = int(input("enter 2nd number"))
+
+for i in range(number1 + 1, number2):
+
+    s = 0
+    sum = i
+    temp = i
+
+    while temp > 0:
+        digit = temp % 10
+        s = s + digit ** 3
+        temp = temp // 10
+
+    if s == sum:
+        print("the given number is armstrong", s)
