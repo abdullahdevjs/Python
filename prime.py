c7de279a8d19e6b6d753b5739b4e57395527c875
Mohammad Abdullah <quizyworld0009@gmail.com>
2026-08-26 23:54:02 +0530

    range of a prime no

--- prime.py
+++ prime.py
@@ -5,3 +5,13 @@ for i in range(2,number):
         break
 else:
     print("given number is prime number")
+
+number1=int(input("enter first number"))
+number2=int(input("enter first number"))
+for i in range(number1+1,number2):
+    for j in range(2,i):
+        if i%j==0:
+            
+            break
+    else:
+        print("prime",i)
